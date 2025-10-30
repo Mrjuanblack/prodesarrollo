e71a575d85da6bb4fd168b1a402124401929afba
Neider08Urbano <jjuly8julianur@gmail.com>
2025-10-30 08:42:47 -0500

    adjust page

--- app/(app)/about-us/page.tsx
+++ app/(app)/about-us/page.tsx
@@ -1,9 +1,12 @@
 import { HeroSimple } from "@/ui/organism";
 import hero_simple from "@/public/hero-simple.svg";
+import { CallToActionSection } from "@/ui/organism/CallToActionSection/CallToActionSection";
+
 export default function AboutUs() {
   return (
     <>
       <HeroSimple title="¿Quiénes somos?" backgroundImage={hero_simple} />
+      <CallToActionSection />
     </>
   );
 }
